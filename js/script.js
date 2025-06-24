75dc698096eefd52ac5d19c71db4008755a7ab4b
Val-Priad <valeriipriadchenko@gmail.com>
2025-06-24 19:55:00 +0200

    lesson 140

--- js/script.js
+++ js/script.js
@@ -4,9 +4,18 @@ const currentYear = new Date().getFullYear();
 copyrightYear.textContent = currentYear;
 
 // MOBILE NAVIGATION
-
 const btnMobileNav = document.querySelector(".btn-mobile-nav")
 const header = document.querySelector(".header")
 btnMobileNav.addEventListener("click", function () {
     header.classList.toggle('nav-open');
 })
+
+// HIDING NAVIGATION FOR MOBILES AFTER USING
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        if (link.classList.contains("main-nav-link")) {
+            header.classList.toggle("nav-open");
+        }
+    })
+});
